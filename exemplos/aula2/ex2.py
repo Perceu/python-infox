a73a47f0a8d6b3fe56eabd12bd825f4105ca603e
Perceu <perceubertoletti@gmail.com>
2018-03-14 13:23:00 -0300

    melhorado alguns exemplos e criado exercicios

---
+++ exemplos/aula2/ex2.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+v1 = 1
+v2 = 1.0
+v3 = 'perceu'
+v4 = True
+v5 = datetime.now()
+
+print(type(v1))
+print(type(v2))
+print(type(v3))
+print(type(v4))
+print(type(v5))
